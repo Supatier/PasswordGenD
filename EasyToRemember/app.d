255fe195eb073dfdda8d3169f0f84ab282cb00c9
Supatier <atisuperati@gmail.com>
2017-12-14 19:30:41 +0700

    Seperate

--- EasyToRemember/app.d
+++ EasyToRemember/app.d
@@ -1,48 +1,6 @@
 #!/usr/bin/env rdmd
 
-import std.algorithm : map;
-import std.array : array;
-import std.ascii : digits, lowercase;
-import std.conv : text;
-import std.random : choice;
-import std.stdio : write, writeln;
-
-struct PrintRandomCharacters {
-        auto symbols = "!@#$%^&*()-_=+[{]}\\|;:\"\',<.>/?".map!text.array;
-        auto uppercaseConsonants = "BCDFGHJKLMNPQRSTVWXYZ".map!text.array;
-        auto lowercaseConsonants = "bcdfghjklmnpqrstvwxyz".map!text.array;
-        auto lowercaseVocals = "aiueo".map!text.array;
-
-        void randUppercase() {
-                auto val = choice(uppercaseConsonants);
-                write(val);
-        }
-
-        void randLowercaseConsonant() {
-                auto val = choice(lowercaseConsonants);
-                write(val);
-        }
-
-        void randLowercaseVocal() {
-                auto val = choice(lowercaseVocals);
-                write(val);
-        }
-
-        void randLowercase() {
-                auto val = choice(lowercase.map!text.array);
-                write(val);
-        }
-
-        void randDigit() {
-                auto val = choice(digits.map!text.array);
-                write(val);
-        }
-
-        void randSymbol() {
-                auto val = choice(symbols);
-                write(val);
-        }
-}
+import gen;
 
 void main() {
         auto printRandomCharacters = new PrintRandomCharacters;
---
+++ EasyToRemember/gen.d
@@ -0,0 +1,46 @@
+module gen;
+
+import std.algorithm : map;
+import std.array : array;
+import std.ascii : digits, lowercase;
+import std.conv : text;
+import std.meta : Alias;
+import std.random : choice;
+import std.stdio : write, writeln;
+
+struct PrintRandomCharacters {
+        auto symbols = "!@#$%^&*()-_=+[{]}\\|;:\"\',<.>/?".map!text.array;
+        auto uppercaseConsonants = "BCDFGHJKLMNPQRSTVWXYZ".map!text.array;
+        auto lowercaseConsonants = "bcdfghjklmnpqrstvwxyz".map!text.array;
+        auto lowercaseVocals = "aiueo".map!text.array;
+
+        void randUppercase() {
+                auto val = choice(uppercaseConsonants);
+                write(val);
+        }
+
+        void randLowercaseConsonant() {
+                auto val = choice(lowercaseConsonants);
+                write(val);
+        }
+
+        void randLowercaseVocal() {
+                auto val = choice(lowercaseVocals);
+                write(val);
+        }
+
+        void randLowercase() {
+                auto val = choice(lowercase.map!text.array);
+                write(val);
+        }
+
+        void randDigit() {
+                auto val = choice(digits.map!text.array);
+                write(val);
+        }
+
+        void randSymbol() {
+                auto val = choice(symbols);
+                write(val);
+        }
+}
